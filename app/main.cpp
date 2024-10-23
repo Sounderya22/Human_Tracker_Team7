1305f8455f0add64aa7112f82ec2cadd16f0e941
Anirudh Swarankar <swarankar.ani24@gmail.com>
2024-10-23 00:46:38 -0400

    Added main of App

--- app/main.cpp
+++ app/main.cpp
@@ -1,6 +1,29 @@
-#include <iostream>
-#include "Detector.hpp"
+// #include <readerwriterqueue.h>
+//
+// int main()
+// {
+//   moodycamel::ReaderWriterQueue<int> q(100);
+// }
+/**
+ * @file  main.cpp
+ * @authors Anirudh Swarankar, Amogha Sunil
+ * @brief C++ source file for main pipeline to run AcmeVision instance
+ * @version 0.1
+ * @date 2024-10-16
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
 
-int main() {
+#include "acme_vision.hpp"
 
+int main(){
+    std::unique_ptr<acmebot::AcmeVision> acmevision = std::make_unique<acmebot::AcmeVision>(0);
+    acmevision->windowControl.showRawStream = false;
+
+
+  acmevision->ProcessCameraFrame();
+
+  return 0;
 }
+
